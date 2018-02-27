4ab89cc831e8dfa6daf3ea822c46c5aaddb11db1
Dmitry Moskowski <me@corpix.ru>
2018-02-27 15:21:08 +0000

    Init

---
+++ firewall.nix
@@ -0,0 +1,146 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  name = "firewall";
+  cfg = config.services."${name}";
+in {
+  options = with types; {
+    services."${name}" = let
+      ruleCriteria = submodule ({ ... }: {
+        options = {
+          ports = mkOption {
+            type = listOf int;
+            default = [ ];
+            example = [ 22 ];
+            description = ''
+              List of ports to use with this rule.
+            '';
+          };
+          protocols = mkOption {
+            type = listOf str;
+            default = [ ];
+            example = [ "tcp" ];
+            description = ''
+              List of protocols to use with this rule.
+            '';
+          };
+          interfaces = mkOption {
+            type = listOf str;
+            default = [ ];
+            example = [ "enp3s0" ];
+            description = ''
+              List of network interfaces to use with this rule.
+            '';
+          };
+        }
+      });
+    in {
+      enable = mkEnableOption "High-level firewall helper service";
+      chains = mkOption {
+        type = submodule ({ ... }: {
+          options = {
+            names = mkOption {
+              type = listOf str;
+              default = [
+                "input"
+                "forward"
+              ];
+              description = ''
+                List of chains to apply policy for.
+              '';
+            };
+
+            policy = mkOption {
+              type = nullOr str;
+              default = "drop";
+              description = ''
+                Default policy for chains.
+              '';
+            };
+          };
+        });
+        default = { };
+        example = { policy = "reject"; };
+        description = ''
+          Firewall chains and policy for them.
+        '';
+      };
+
+      accept = mkOption {
+        type = listOf ruleCriteria;
+        default = [];
+        example = [ { ports = [ 22 ]; protocols = [ "tcp" ]; interfaces = [ "enp3s0" ]; } ];
+        description = ''
+          List of attrs with port number, protocols and interface to accept.
+        '';
+      };
+
+      reject = mkOption {
+        type = listOf ruleCriteria;
+        default = [];
+        example = [ { ports = [ 22 ]; protocols = [ "tcp" ]; interfaces = [ "enp3s0" ]; } ];
+        description = ''
+          List of attrs with port number, protocols and interface to reject traffic on.
+        '';
+      };
+
+      rejectSilently = mkOption {
+        type = listOf ruleCrtiteria;
+        default = [];
+        example = [ { ports = [ 22 ]; protocols = [ "tcp" ]; interfaces = [ "enp3s0" ]; } ];
+        description = ''
+          List of attrs with port number, protocols and interface to reject traffic on without logging.
+        '';
+      };
+    };
+  };
+
+  config = with lib; let
+    rejectChain = silent:
+      if silent
+        then "nixos-fw-refuse"
+        else "nixos-fw-log-refuse";
+    foldRecord = fn: record:
+      foldl
+        (acc: interfaces:
+          foldl
+            (acc: protocols:
+              foldl
+                (acc: ports: acc ++ [ports])
+                acc
+                protocols)
+            acc
+            interfaces)
+        []
+        (map
+          (interface: map
+            (protocol: map (port: fn port protocol interface) record.ports)
+            (attrByPath ["protocols"] [ "tcp" "udp" ] record))
+          (attrByPath ["interfaces"] [ "" ] record));
+    mkPolicy = policy: chain:
+      "ip46tables -P ${chain} ${policy}";
+    mkReject = port: protocol: interface: silent:
+      "ip46tables -A nixos-fw ${optionalString (interface != "") "-i ${interface}"} -p ${protocol} --dport ${toString port} -j ${rejectChain silent}";
+    mkAccept = port: protocol: interface:
+      "ip46tables -A nixos-fw ${optionalString (interface != "") "-i ${interface}"} -p ${protocol} --dport ${toString port} -j nixos-fw-accept";
+  in mkIf cfg.enable {
+    networking.firewall = {
+      enable = true;
+      extraCommands =
+        mkAfter
+        (concatStringsSep
+          "\n"
+          (flatten (
+            (map (foldRecord mkAccept) cfg.accept)
+            ++ (map (v: map (fn: fn false) (foldRecord mkReject v)) cfg.reject)
+            ++ (map (v: map (fn: fn true) (foldRecord mkReject v)) cfg.rejectSilently)
+            ++ (
+              if cfg.chains.policy != null
+              then (map (mkPolicy cfg.chains.policy) cfg.chains.names)
+              else []
+            ))));
+      };
+  };
+}
---
+++ infotty.nix
@@ -0,0 +1,78 @@
+{ config, lib, pkgs, ... }:
+
+with builtins;
+with lib;
+
+let
+  name = "infotty";
+  cfg = config.services."${name}";
+in {
+  options = with types; {
+    services."${name}" = {
+      enable = mkEnableOption "Reports information about host.";
+      tty = mkOption {
+        type = int;
+        default = 12;
+        description = ''
+          TTY number to show information on.
+        '';
+      };
+      interval = mkOption {
+        type = str;
+        default = "5s";
+        description = ''
+          Pause between information updates.
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.infotty = {
+      enable = true;
+      description = "reports system information to specified TTY";
+      wantedBy = [ "multi-user.target" ];
+      script = with pkgs; ''
+        set -e
+
+        export PATH=${makeBinPath [ coreutils findutils eject openssh osquery ]}
+
+        while true
+        do
+          # Clear sequence
+          echo -e '\033\0143'
+
+          echo Network interface details:
+          # Errors dev/null'ed because of this shit:
+          # I0227 00:22:06.202585 16250 init.cpp:612] Cannot start extension manager: Extensions disabled
+          # It does not react to --disable_extensions or --disable_kernel flags, so fuck it
+          osqueryi 'select interface, mac, mtu from interface_details;' 2> /dev/null
+
+          echo
+          echo Network interface addresses:
+          osqueryi 'select interface, address, mask from interface_addresses;' 2> /dev/null
+
+          echo
+          echo Host SSH keys fingerprints:
+          ls /etc/ssh/ssh_host_*.pub | xargs -I'{}' ssh-keygen -E sha256 -lf '{}' | column -t
+
+          echo
+          echo Failed systemd units:
+          systemctl list-units --state=failed
+
+          echo
+          echo -n 'Generated at(updates every ${cfg.interval}): '
+          date
+          uptime
+
+          sleep ${cfg.interval}
+        done > /dev/tty${toString cfg.tty} 2>&1
+      '';
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        NoNewPrivileges = "yes";
+      };
+    };
+  };
+}
